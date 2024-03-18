826b0e18f5643ce89b2957f5fba2f7aeaa0b7923
seekingmeanings <git-j@mail.o-family.de>
2024-03-18 07:53:14 +0000

    IO extender class

--- src/activator.py
+++ src/activator.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+class ExdenderBoard:
+    def __init__(self, address):
+        self.address = address
+        self.leds = []
+
+
 class LED:
     def __init__(self, pin):
         self.state = False
