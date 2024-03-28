09ed1aad023da7f250c39cffec4009dfad7190a6
seekingmeanings <git-j@mail.o-family.de>
2024-03-28 20:34:38 +0000

    server structure

--- src/board.py
+++ src/board.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import json
+
 from activator import LED
 
 class Board:
@@ -9,3 +11,29 @@ class Board:
     class Segment:
         def __init__(self) -> None:
             pass
+
+
+class BoardConfig:
+    def __init__(self, config_file: str) -> None:
+        self.config_file = config_file
+        self.config = self.load_config()
+
+    def load_config(self) -> dict:
+        with open(self.config_file, 'r') as file:
+            return json.load(file)
+        
+    def _get_board(self) -> Board:
+        for board in self.boards:
+            if board['type'] not in self.config['outputs']:
+                raise ValueError(f'Board type {board["type"]} not found in config')
+            lol = self.config['boards']
+
+
+
+    def get_led(self, led_id: int) -> LED:
+        led = self.config['leds'][led_id]
+        return LED(led['pin'], led['color'])
+
+    def get_segment(self, segment_id: int) -> Board.Segment:
+        segment = self.config['segments'][segment_id]
+        return Board.Segment()
