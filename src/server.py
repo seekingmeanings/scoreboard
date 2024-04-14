16f19584db6e160a8f957a0c56aa28c7a0fa3498
seekingmeanings <git-j@mail.o-family.de>
2024-04-14 22:19:49 +0200

    delete socket stuff

--- src/server.py
+++ src/server.py
@@ -15,3 +15,51 @@ from dataclasses import dataclass
 import json
 # import re
 
+class ContentRessource(Resource, AccessCheck):
+    pass
+
+class ConfigRessource(Resource, AccessCheck):
+    pass
+
+
+class StateAccess(ContentRessource):
+    
+    def get(self):
+        return
+
+
+
+
+class BoardServer:
+    def __init__(self):
+        lg.debug("server instance init called")
+
+        self.app = Flask("SERVER NAME")
+        self.api = Api(self.app)
+
+        lg.debug("adding resource points")
+
+
+    def run(self):
+        lg.info("starting server")
+        self.app.run()
+
+
+
+
+
+
+
+def main():
+
+    lg.basicConfig(level="DEBUG")
+
+    lg.dubg(f"starting instance")
+    server_instance = BoardServer(
+        config=None,
+    )
+
+
+
+if __name__ == "__main__":
+    main()
