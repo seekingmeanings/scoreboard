8bca4afc3a3b1af44554bfb9e7951335730ce9e2
seekingmeanings <git-j@mail.o-family.de>
2024-05-10 23:09:22 +0200

    reorganized some files

--- src/helper/race_condition.py
+++ src/helper/race_condition.py
@@ -1,22 +1,26 @@
 from time import gmtime
 from threading import RLock
+from functools import wraps
+
 
 class LockedTracking:
     def __init__(self) -> None:
         self._last_access = None
         self._lock = RLock()
 
-    # TODO: is this supposed to be static?
     @staticmethod
     def locked_access(_func=None, *, track=True):
         def exec_wrap(func):
+            @wraps
             def wrapper(*args, **kwargs):
                 wrapped_self = args[0]
                 with wrapped_self._lock:
                     if track:
                         wrapped_self.last_access = gmtime()
                     return func(*args, **kwargs)
+
             return wrapper
+
         if _func is None:
             return exec_wrap
         else:
