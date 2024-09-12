de610cb86081cf5a3a033ffeaddf7d3d5bbd9958
seekingmeanings <git-j@mail.o-family.de>
2024-09-12 11:38:08 +0200

    accidentally sleeping with factor 1000

--- src/plugins/time/time.py
+++ src/plugins/time/time.py
@@ -1,43 +1,56 @@
-from src.helper.conf_loader import ConfigLoader
-from src.helper.board_linker import BoardLinker
+import os
 
-from public import public
 import time
 import asyncio
 
+from typing import Optional
 import logging as lg
 
+import tomllib
 
-class TimePlugin(ConfigLoader, BoardLinker):
+
+class TimePlugin:
     # TODO: get the board linker to link outside of the class for all once \
     # TODO: and then populate classes like this with (classwrapper???)
 
     # TODO: move ConfigLoader and other parents to common plugin parent
     def __init__(self, board, config_file: str):
-        ConfigLoader.__init__(self, config_file)
-        # TODO: some better dynamic linking
-        BoardLinker.__init__(self, board=board)
-        self._time = None
-        self._active = True
+        # TODO: use Config class
+        self.config: dict = dict()
+        self.config_file: str = os.path.join(os.path.dirname(__file__), config_file)
+        with open(self.config_file, 'rb') as f:
+            self.config = tomllib.load(f)
 
-    @public
+        print(f"loaded config: {self.config} from {self.config_file}")
+
+        # linked board resource
+        self.board = board
+
+        self._time: Optional[time.struct_time] = None
+        self._active: bool = False
+
     def set_time(self, time_to_set: time.struct_time) -> None:
+        lg.debug(f"setting time to {time_to_set}")
         for digit_name, digit_obj in self.config["digits"].items():
             format_str, index = (digit_obj["strftime"]["format"],
                                  digit_obj["strftime"]["index"])
             char = str(time.strftime(format_str, time_to_set))[index]
             self.board.display_char(digit_name, char)
-            lg.debug(f"setting time component: {digit_name}: {char}")
 
-    async def update_time(self):
+    def update_time(self):
         last = time.gmtime()
         while self._active:
             now = time.gmtime()
             if time.mktime(now) - time.mktime(last) >= 1:
                 self.set_time(now)
                 last = now
-            await asyncio.sleep(self.config["update_interval"])
+            time.sleep(float(self.config['time']["update_interval"])/1000)
 
-    @public
+        lg.debug("time update loop stopped??")
+
     def run(self):
-        asyncio.run(self.update_time())
+        self._active = True
+        self.update_time()
+
+    def stop(self):
+        self._active = False
