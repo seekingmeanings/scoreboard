318edc2c090ebe6b7c37cf52a789905798b12b52
seekingmeanings <git-j@mail.o-family.de>
2024-09-12 21:20:33 +0200

    Update plugin.py
    comment out not defined stuff

--- src/helper/plugin.py
+++ src/helper/plugin.py
@@ -12,14 +12,14 @@ class Plugin:
 
 # from time plugin
 # TODO: make this an actual plugin class and this an instance
-class Plugin:
-    def __init__(self, plugin_conf, resources):
-        self.external_resources = resources
-
-        # init the plugin
-        self._plugin = TimePlugin(
-            self.external_resources['board'],
-            self.external_resources['config']['plugins']['p']['time']['config_file']
-        )
-
-        self.external_resources['time'] = self._plugin
+#class Plugin:
+#    def __init__(self, plugin_conf, resources):
+#        self.external_resources = resources
+#
+#        # init the plugin
+#        self._plugin = TimePlugin(
+#            self.external_resources['board'],
+#            self.external_resources['config']['plugins']['p']['time']['config_file']
+#        )
+#
+#        self.external_resources['time'] = self._plugin
