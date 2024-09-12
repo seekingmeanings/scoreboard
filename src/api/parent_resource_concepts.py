c9731e5b6a43f04ae88616673074055871536434
seekingmeanings <git-j@mail.o-family.de>
2024-09-12 21:14:49 +0200

    - logging properly - controls can be added with the decorator

--- src/api/parent_resource_concepts.py
+++ src/api/parent_resource_concepts.py
@@ -1,11 +1,14 @@
 import importlib
 
+from typing import Union, List
+
 import flask_restful
 import functools
 import ast
-import logging as lg
+import logging
 
 from src.helper import get_wrapped_classes as wrap_scan
+from src.things.scoreboard.controls import Control, ControlManager
 
 
 class ApiEndpointManager:
@@ -34,21 +37,24 @@ class ApiEndpointManager:
     def __init__(
             self,
             api: flask_restful.Api,
-            #endpoint_modules: list,
-            resources
+            resources,
+            control_manager: ControlManager,
     ):
+        self.lg = logging.getLogger(self.__class__.__name__)
         self.test = None
         self.api = api
         self.resource = resources
+        self.control_manager = control_manager
 
-    @staticmethod
-    def import_endpoint_module(module: str) -> None:
+    def import_endpoint_module(self, module: str) -> None:
 
-        # TODO: every file has to be imported, or the __init__.py has to take care
-        lg.debug(f"importing module: {module}")
+        # TODO: every file has to be imported
+        self.lg.debug(f"importing module: {module} in api endpoint manager")
+        self.lg.warning(f"the module needs a sufficient __init__.py file to recognize api endpoints")
         importlib.import_module(module)
 
-    def auto_add_endpoints_not_needed(self) -> None:
+    @staticmethod
+    def auto_add_endpoints_not_needed() -> None:
         import src.api.endpoints
         print("added endpoints module")
         endpoints = wrap_scan.find_wrappers_in_module_path(src.api.endpoints)
@@ -57,15 +63,12 @@ class ApiEndpointManager:
             for decorator in [ast.parse(dec_str, mode='eval') for dec_str in endpoint['decorators']]:
                 print(decorator)
 
-    # just api, make resource another one
-    # so all resource is sepereate and tihs is just api class
-    # so it can be a subclass of flask_restful.api ??????????????s
     def add_resources(self,
                       resources: dict = None,
                       url: str = None,
+                      control: Union[Control, List[Control]] = None,
                       ):
         sself = self
-        print("called the decorator")
 
         def decorator(cls):
             class WrappedClass(cls):
@@ -78,15 +81,25 @@ class ApiEndpointManager:
                                 name,
                                 sself.resource[res_name]
                             )
-
             if url:
                 # does it init itself??
-                print(url)
-                print(WrappedClass)
+                self.lg.debug(f"adding resource {cls} to {url} as {WrappedClass}")
                 sself.api.add_resource(
                     WrappedClass,
                     sself.api.base_url + url,
                     endpoint=f"{cls.__name__.lower()}_endpoint"
                 )
+
+            if control:
+                if isinstance(control, list):
+                    for c in control:
+                        c.api_call.make_call(url)
+
+                        self.lg.debug(f"adding control {c}")
+                        self.control_manager.add_control(c)
+                else:
+                    control.api_call.make_call(url)
+                    self.lg.debug(f"adding control {control}")
+                    self.control_manager.add_control(control)
 
         return decorator
